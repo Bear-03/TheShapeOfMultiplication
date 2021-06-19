ec64feac2970cc93a930b9c7f540fcfe5cbe3825
Bear-03 <bear03.contact@gmail.com>
2021-06-20 01:35:28 +0200

    Make Circle a singleton and add static variables and methods to clean code
    The nodes' diameter is now a static property, as the value will not differ among nodes.
    For the same reason, Node.recalculateDiameter() is now static as well, among other shared values.

--- src/scripts/index/classes.js
+++ src/scripts/index/classes.js
@@ -47,54 +47,50 @@ export class CustomCanvas extends p5 {
 }
 
 class Node {
+	static diameter = 0;
+	/* Defined based on the relation between the man circle diameter
+	and the node diameter, in order to make it responsive.
+
+	i.e. The min node diameter is 0.01 * this.circle.diameter ...
+	*/
+	static minDiameterRelation = 0.01;
+	static maxDiameterRelation = 0.03;
+
 	/**
 	 * @param {p5} s
-	 * @param {Circle} circle main circle
 	 * @param {number} angle angle between the position vector of the node and the horizontal
 	 */
-	constructor(s, circle, angle) {
+	constructor(s, angle) {
 		this.s = s;
-		this.circle = circle;
 		this.angle = angle;
-
-		this.diameter = 0;
-
-		/* Defined based on the relation between the man circle diameter
-		and the node diameter, in order to make it responsive.
-
-		i.e. The min node diameter is 0.01 * this.circle.diameter ...
-		*/
-		this.minDiameterRelation = 0.01;
-		this.maxDiameterRelation = 0.03;
-	}
-
-	resize() {
-		this.recalculateDiameter();
+		this.position = this.s.createVector(0, 0);
 	}
 
 	draw() {
-		const position = this.position;
-
 		this.s.noStroke();
 		this.s.fill(255);
-		this.s.circle(position.x, position.y, this.diameter);
+		this.s.circle(this.position.x, this.position.y, Node.diameter);
 	}
 
-	recalculateDiameter() {
-		this.diameter = this.circle.diameter * clampNumber(this.minDiameterRelation, 1 / this.circle.nodeCount, this.maxDiameterRelation);
+	static recalculateDiameter() {
+		Node.diameter = Circle.instance.diameter * clampNumber(this.minDiameterRelation, 1 / Circle.instance.nodeCount, this.maxDiameterRelation);
 	}
 
-	get position() {
-		return this.s.createVector(Math.cos(this.angle), Math.sin(this.angle)).mult(this.circle.diameter / 2);
+	recalculatePosition() {
+		this.position = this.s.createVector(Math.cos(this.angle), Math.sin(this.angle)).mult(Circle.instance.diameter / 2);
 	}
 }
 
+// Singleton
 export class Circle {
 
 	/**
 	 * @param {CustomCanvas} s
 	 */
 	constructor(s) {
+		if (!Circle.instance) Circle.instance = this;
+		else return Circle.instance;
+
 		this.s = s;
 
 		this.strokeWeight = 2;
@@ -115,9 +111,17 @@ export class Circle {
 
 	resize() {
 		this.recalculateDiameter();
-		this.resizeNodes();
+
+		/* The circle radius, and thus, the node position, will depend on
+		the node diameter, so that must be calculated first */
+		Node.recalculateDiameter();
+
 		// Subtract the node diameter so there isn't overflow
-		this.diameter -= this.nodes[0].diameter;
+		this.diameter -= Node.diameter;
+
+		/* After the main circle and nodes have been resized,
+		the position can be calculated */
+		this.recalculateNodesPosition();
 	}
 
 	/**
@@ -141,19 +145,17 @@ export class Circle {
 		const angleBetweenNodes = 2 * Math.PI / this.nodeCount;
 
 		for (let angleTraveled = 0; angleTraveled < 2 * Math.PI; angleTraveled += angleBetweenNodes)
-			this.nodes.push(new Node(this.s, this, angleTraveled));
+			this.nodes.push(new Node(this.s, angleTraveled));
 	}
 
-	resizeNodes() {
-		for (const node of this.nodes)
-			node.resize();
+	recalculateNodesPosition() {
+		for (const node of this.nodes) node.recalculatePosition();
 	}
 
 	/**
-	 * Draws all the circles in the
+	 * Draws all the circles in the canvas
 	 */
 	drawNodes() {
-		for (const node of this.nodes)
-			node.draw();
+		for (const node of this.nodes) node.draw();
 	}
 }
