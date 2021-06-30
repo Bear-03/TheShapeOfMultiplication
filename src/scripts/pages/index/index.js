3f1e295a774ff7968cd0653cdbe322cd011b34b9
Bear-03 <bear03.contact@gmail.com>
2021-06-30 21:10:10 +0200

    (Canvas) Fixed coordinate system
    The origin was centered correctly but Y increased downwards because
    it wasn't flipped.

--- src/scripts/pages/index/index.js
+++ src/scripts/pages/index/index.js
@@ -17,7 +17,8 @@ const sketch = async (c) => {
 	const lineColors = generateGradientArray(
 		c,
 		maxNodeCount,
-		["#B9E3C6", "#59C9A5", "#D81E5B", "#23395B", "#FFFD98"]
+		//["#B9E3C6", "#59C9A5", "#D81E5B", "#23395B", "#FFFD98"]
+		["#AFCBFF", "#254441", "#43AA8B", "#B2B09B", "#EF3054"]
 	);
 
 	const circle = new Circle(c, lineColors);
@@ -39,7 +40,10 @@ const sketch = async (c) => {
 
 	c.draw = () => {
 		c.clear();
+		// Moves the origin to the center of the canvas
 		c.translate(c.width/2, c.height/2);
+		// flips the y values so y increases "up"
+		c.scale(1, -1);
 
 		circle.draw();
 	};
