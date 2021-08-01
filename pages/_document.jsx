88c19da869059423aeba32640e0f5b71bf39cbed
Bear-03 <bear03.contact@gmail.com>
2021-08-01 03:14:46 +0200

    (Accessibility) Add lang attribute to html element

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class AppDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
 					<meta
 						name="description"
